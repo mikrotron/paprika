9ba6bd1395838341ce0da0510437a92f6a53c8a8
Mike Polowick <mike_polowick@acl.com>
2019-06-28 09:43:28 -0700

    eslint fix

--- packages/Button/stories/Button.stories.js
+++ packages/Button/stories/Button.stories.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { storiesOf, addParameters } from "@storybook/react";
+import { storiesOf } from "@storybook/react";
 import { withKnobs } from "@storybook/addon-knobs";
 
 import Showcase from "./examples/Showcase";
