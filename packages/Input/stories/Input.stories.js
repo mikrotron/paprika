bbcb33734264eb5a325e3c29f5fa93f087c8d38b
Adam Biglow <adam_biglow@acl.com>
2019-06-28 13:10:49 -0700

    showing code 🙀

--- packages/Input/stories/Input.stories.js
+++ packages/Input/stories/Input.stories.js
@@ -8,6 +8,7 @@ import InfoIcon from "@paprika/icon/lib/InfoCircle";
 
 import Input from "../src";
 import InputStoryWrapper from "./assets/InputStoryWrapper";
+import RenderShowCode from "./assets/RenderShowCode";
 
 import SizesStory from "./examples/Sizes";
 import WithContentStory from "./examples/WithContent";
@@ -25,22 +26,25 @@ const iconSelections = {
 storiesOf("Forms | Input", module)
   .addDecorator(withKnobs)
   .addDecorator(InputStoryWrapper)
-  .add("Showcase", ({ onChange, onClear, value }) => (
-    <Input
-      onChange={onChange}
-      onClear={onClear}
-      value={value}
-      size={select("size", ShirtSizes.DEFAULT, "medium")}
-      placeholder={text("placeholder", "Enter some text")}
-      icon={iconSelections[select("icon", Object.keys(iconSelections), null)]}
-      hasClearButton={boolean("hasClearButton", false)}
-      isDisabled={boolean("isDisabled", false)}
-      isReadOnly={boolean("isReadOnly", false)}
-      hasError={boolean("hasError", false)}
-      type={select("type", ["password", "text"], "text")}
-      a11yText={text("a11yText", "")}
-    />
-  ))
+  .add("Showcase", ({ onChange, onClear, value }) => {
+    const el = (
+      <Input
+        onChange={onChange}
+        onClear={onClear}
+        value={value}
+        size={select("size", ShirtSizes.DEFAULT, "medium")}
+        placeholder={text("placeholder", "Enter some text")}
+        icon={iconSelections[select("icon", Object.keys(iconSelections), null)]}
+        hasClearButton={boolean("hasClearButton", false)}
+        isDisabled={boolean("isDisabled", false)}
+        isReadOnly={boolean("isReadOnly", false)}
+        hasError={boolean("hasError", false)}
+        type={select("type", ["password", "text"], "text")}
+        a11yText={text("a11yText", "")}
+      />
+    );
+    return <RenderShowCode>{el}</RenderShowCode>;
+  })
   .add("Variations", ({ onChange, onClear, value }) => {
     const defaultProps = {
       value,
---
+++ packages/Input/stories/assets/RenderShowCode.js
@@ -0,0 +1,31 @@
+import React from "react";
+import SyntaxHighlighter from "react-syntax-highlighter";
+
+const RenderShowCode = props => {
+  const { children } = props;
+
+  const el = children;
+  console.log(children);
+
+  const elName = el.type.displayName;
+  const keys = Object.keys(el.props);
+
+  const elProps = keys.map(key => {
+    return `${key}="${el.props[key]}"`;
+  });
+
+  const tab = "  ";
+
+  const codeString = `<${elName}\n${tab}${elProps.map(prop => `${prop}\n`).join(tab)}></${elName}>`;
+
+  return (
+    <div>
+      <div>{el}</div>
+      <div>
+        <SyntaxHighlighter language="javascript">{codeString}</SyntaxHighlighter>
+      </div>
+    </div>
+  );
+};
+
+export default RenderShowCode;
